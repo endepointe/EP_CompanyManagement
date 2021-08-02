aec224c7f6e602efcc8919de3a5f311e64979b6c
endepointe <endepointe@gmail.com>
2021-08-02 03:18:22 -0700

    np

--- Client/Program.cs
+++ Client/Program.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
 
 namespace Client
 {
@@ -15,10 +18,19 @@ namespace Client
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
+
+            builder.Services    
+                .AddBlazorise(options => 
+                {
+                    options.ChangeTextOnKeyPress = true;
+                })
+                .AddBootstrapProviders()
+                .AddFontAwesomeIcons();
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+            builder.RootComponents.Add<App>("#app");
+            
             await builder.Build().RunAsync();
         }
     }
